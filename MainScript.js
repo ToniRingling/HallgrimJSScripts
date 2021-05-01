646520e13f9175495a2601e92724ad6b1762f1ce
ToniRingling <83506258+ToniRingling@users.noreply.github.com>
2021-05-01 22:07:54 +0200

    Removed testing log command.
    Confirmed via the console, that the problem is, that HGExecuteTask is only used for the first task on the page. The problem as such is with HGVersionCheckAndExecute.

--- MainScript.js
+++ MainScript.js
@@ -22,8 +22,6 @@ function HGExecuteTask(taskCode){
 		  HGInstanceGlobal = 0; // first instance, set number to 0
 		}
 		var HGInstance = HGInstanceGlobal;
-		
-		console.log("EXECUTE INSTANCE " + HGInstance);
 
 		HGInstanceGlobal = HGInstanceGlobal + 1;
 
